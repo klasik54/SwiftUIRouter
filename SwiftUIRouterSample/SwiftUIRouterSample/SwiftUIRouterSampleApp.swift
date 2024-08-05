eff266260bb54551e63efe84cf29170cdf362eb7
klasik54 <matus.klasovity@gmail.com>
2024-08-05 20:05:34 +0200

    feat: Create sample project

---
+++ SwiftUIRouterSample/SwiftUIRouterSample/SwiftUIRouterSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRouterSampleApp.swift
+//  SwiftUIRouterSample
+//
+//  Created by Matúš Klasovitý on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRouterSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
